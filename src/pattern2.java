2e2ea04f07d8b389c594361abf5680193092d6ba
Mubasher327 <126305020+Mubasher327@users.noreply.github.com>
2023-04-03 16:11:15 +0500

    03-04-2023

---
+++ src/pattern2.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class pattern2 {
+
+
+        public static void main(String[] args) {
+
+            Scanner scanner = new Scanner(System.in);
+            int num = 0;
+            String num1=null;
+            boolean isValid = false;
+
+            while (!isValid) {
+                System.out.print("Enter a number: ");
+
+                if (scanner.hasNextInt()) {
+                    num = scanner.nextInt();
+                    isValid = true;
+                } else if (scanner.hasNextLine()) {
+                    num1= scanner.nextLine();
+                    isValid = false;
+                } else {
+                    System.out.println("Input is not a number. Please try again.");
+                    scanner.next();
+                }
+            }
+
+            System.out.println("The number entered is: " + num);
+            scanner.close();
+
+            for (int i = 1; i <= num; i++) {
+                for (int j = num; j >=i; j--) {
+                    System.out.print(" * ");
+                }
+                System.out.println();
+            }
+
+
+
+        }
+
+}
---
+++ src/pattern3.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class pattern3 {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        int num = 0;
+        String num1=null;
+        boolean isValid = false;
+
+        while (!isValid) {
+            System.out.print("Enter a number: ");
+
+            if (scanner.hasNextInt()) {
+                num = scanner.nextInt();
+                isValid = true;
+            } else if (scanner.hasNextLine()) {
+                num1= scanner.nextLine();
+                isValid = false;
+            } else {
+                System.out.println("Input is not a number. Please try again.");
+                scanner.next();
+            }
+        }
+
+        System.out.println("The number entered is: " + num);
+        scanner.close();
+
+        for (int i = 1; i <= num; i++) {
+            for (int j = 1; j >=num; j++) {
+                System.out.print(" * ");
+            }
+            System.out.println();
+        }
+
+
+
+    }
+
+}
